30c14c9972df197083b0c0ec30f16ce342b197ab
trtahhh <tuananh15213@gmail.com>
2024-09-23 08:30:02 +0700

    BaitapTuan1

---
+++ Quan Ly Ban Hang/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quan_Ly_Ban_Hang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<SanPham> dSSP = new List<SanPham>();   
+            int n = 0;
+            Console.Write("Nhap so luong san pham: ");  n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"San pham thu {i + 1}: ");
+                SanPham spham = new SanPham();
+                spham.Input();
+                dSSP.Add(spham);
+            }
+            TongGia(dSSP);
+
+            Console.ReadKey();
+        }
+        public static void TongGia(List<SanPham> dSSP)
+        {
+            var TongGia = dSSP.Sum(s => s.Gia);
+            Console.WriteLine("Tong so tien la: " + TongGia);
+        }
+    }
+}
---
+++ Quan Ly Ban Hang/SanPham.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quan_Ly_Ban_Hang
+{
+    internal class SanPham
+    {
+        private string ten;
+        private int gia;
+
+        public string Ten { get; set; }
+        public int Gia { get; set; }
+
+        public SanPham(){}
+        public SanPham(string ten, int gia)
+        {
+            this.ten = ten;
+            this.gia = gia;
+        }
+
+        public void Input()
+        {
+            Console.Write("Nhap ten san pham: ");
+            Ten = Console.ReadLine();
+            Console.Write("Nhap gia san pham: ");
+            Gia = int.Parse(Console.ReadLine());
+        }
+
+        public void Output()
+        {
+            Console.WriteLine($"Ten: {Ten}, \nGia: {Gia}");
+        }
+    }
+}
